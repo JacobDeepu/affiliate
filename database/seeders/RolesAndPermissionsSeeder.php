66ae4b9d74fc3739e07c250bd19f9d1a9687200d
Jacob Deepu <d2jdeepu@gmail.com>
2024-03-21 23:07:37 +0530

    Create a RolesAndPermissionsSeeder

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $arrayOfPermissionNames = [
+            'view an affiliate', 'verify an affiliate',
+        ];
+        $permissions = collect($arrayOfPermissionNames)->map(function ($permission) {
+            return ['name' => $permission, 'guard_name' => 'web'];
+        });
+
+        Permission::insert($permissions->toArray());
+
+        Role::create(['name' => 'Super Admin']);
+        Role::create(['name' => 'Admin']);
+        Role::create(['name' => 'Affiliate']);
+    }
+}
